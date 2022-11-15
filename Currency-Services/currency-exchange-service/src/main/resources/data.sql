c6c2208dd94b1b90e5960b563be2f748f94eff47
Audry Kibonge <audrykibs@live.fr>
2022-11-15 14:11:34 -0600

    currency microservice completed

---
+++ Currency-Services/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+       values(100001, 'USD', 'INR', 65,'');
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+       values(100002, 'YUA', 'INR', 35,'');
+       insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+              values(100003, 'EUR', 'INR', 75,'');
+              insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+                     values(100004, 'AUD', 'INR', 25,'');
